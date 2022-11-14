f7f0f431bc9e9065e1e1f2ab7dc8db73548517e7
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-11-14 14:06:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,4 +3,28 @@ using namespace std;
 
 int main() {
    return 0;
+   float a=0, b=0, x=0, y=0;
+    cin >> a >> b >> x >> y;
+    float z=0;
+    if(x<0 && y>0)
+    {
+        z= a*x - b*y;
+    }else if (x>=0 && y<=0)
+    {
+        z= a*x*x - b*y;
+    }else
+    {
+        z=a*x + b*y*y;
+    }
+    cout << z;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
